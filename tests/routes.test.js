6f786b920854c28b2e0fd2ccdc4dec5d2ae77461
Alpha Olomi <alphaolomi@gmail.com>
2019-12-30 04:13:42 +0300

    add jest testing

---
+++ tests/routes.test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+// const app = require("../app");
+
+// describe('Post Endpoints', () => {
+//   it('should create a new post', async () => {
+//     const res = await request(app)
+//       .post('/api/posts')
+//       .send({
+//         userId: 1,
+//         title: 'test is cool',
+//       })
+//     expect(res.statusCode).toEqual(201)
+//     expect(res.body).toHaveProperty('post')
+//   })
+// })
+
+describe("another sample Test", () => {
+  it("should test that true === true", () => {
+    expect(true).toBe(true);
+  });
+});
